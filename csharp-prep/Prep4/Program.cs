a60c916e3906e5303fdcf1e0aad0e3ffaf76223f
Jarilyjon <jarilyjon@gmail.com>
2023-01-11 22:54:14 +0000

    Completed lists code

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int numberEntered = -1;
+
+        while (numberEntered != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            numberEntered = int.Parse(userInput);
+
+            if (numberEntered != 0)
+            {
+                numbers.Add(numberEntered);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+
     }
 }
