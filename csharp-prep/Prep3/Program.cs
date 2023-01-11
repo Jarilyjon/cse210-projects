72cdde1d38f15368e8ae79363df100bbb266bf44
Jarilyjon <jarilyjon@gmail.com>
2023-01-11 22:03:38 +0000

    Created variables and input/output code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,39 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        int count = 0;
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        Console.WriteLine($"What is the magic number? {number}");
+        int guess = -1;
+        
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            count++;
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You made {count} guess(es).");
+
+            }
+        } 
+
     }
 }
